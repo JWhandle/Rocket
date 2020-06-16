0e169387dc4b23003465b03c628ca2c51a3b75f4
JWhandle <wanfo00350@loswego.k12.or.us>
2020-06-16 19:28:04 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,25 @@
+class Rocket:
+
+  #Constructor Method
+  #"yellow,"blue","red"
+  def __init__(self,fuelAmount,bodyColorInput,frameTypeInput,sizeInput,typeinput):
+    self.fuelAmount=fuelAmount
+    self.bodyColor=bodyColorInput
+    self.frame=frameTypeInput
+    self.size=sizeInput
+    self.type=typeinput
+  
+#Drive Method
+def fly(self):
+  print("We are flying!")
+  def Crash(self):
+    print("Oh no!")
+
+  def speedUp(self):
+    print("Here we go!")
+
+
+
+JasperRocket=Rocket(200,"red","I dunno","500","Rocket")
+
+print(JasperRocket.fuelAmount)
